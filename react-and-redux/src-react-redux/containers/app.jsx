ac98c0d2068363722116eb453cbc9dc811169bd8
HelenTim <3427439707@qq.com>
2020-02-06 22:10:40 +0800

    Update app.jsx

--- react-and-redux/src-react-redux/containers/app.jsx
+++ react-and-redux/src-react-redux/containers/app.jsx
@@ -6,4 +6,4 @@ import Counter from '../components/counter'
 export default connect(
   state =>({count:state}),
   {increment,decrement}
-)(Counter); //connect返回一个函数
+)(Counter); //connect返回一个函数  //自注：Counter组件会接收 这里的count、increment、decrement 三个属性 以便使用。
