e8d1ff2f45c55505f06d7ab02e92a35ebada6826
HelenTim <3427439707@qq.com>
2020-02-06 12:16:48 +0800

    Update store.js

--- react-redux/src-redux/redux/store.js
+++ react-redux/src-redux/redux/store.js
@@ -1,5 +1,6 @@
 import {createStore} from 'redux'
 import {counter} from './reducers'
 //生成一个store对象
+// 自注：生成 redux事件订阅分发机制对象 
 const store = createStore(counter) //内部会第一次调用reducer函数，得到初始state为0
 export default store
