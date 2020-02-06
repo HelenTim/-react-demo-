97b165e5f450b137011f01cabb0b670859d56130
HelenTim <3427439707@qq.com>
2020-02-06 10:26:31 +0800

    Update reducers.js

--- react-redux/src-redux/redux/reducers.js
+++ react-redux/src-redux/redux/reducers.js
@@ -3,7 +3,8 @@ import {
   INCREMENT,
   DECREMENT
 } from './action-types'
-export function counter(state = 0, action) {
+export function counter(state = 0, action) {  // 自注：reducer模块的作用是  利用state以及action函数  来返回新的数据(state)。
+                                              // 注意是返回新的数据(state)不是修改原来的数据(state)。        
   console.log('counter()',state,action)
   switch (action.type) {
     case INCREMENT:
