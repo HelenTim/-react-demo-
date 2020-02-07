035e7b8095ccd4b74d6adbe019605613464c172e
HelenTim <3427439707@qq.com>
2020-02-07 10:02:28 +0800

    Update store.js

--- react-and-redux/src-redux-thunk/redux/store.js
+++ react-and-redux/src-redux-thunk/redux/store.js
@@ -5,6 +5,7 @@ import {counter} from './reducers'
 //生成一个store对象
 const store = createStore(
   counter,
-  composeWithDevTools(applyMiddleware(thunk))  //应用上异步中间件
+  // applyMiddleware(thunk) 的意思是 应用 thunk中间件
+  composeWithDevTools(applyMiddleware(thunk))  //应用上异步中间件  // 自注：这里的意思是 让中间件里的数据和开发工具同步。异步数据可以被开发工具监听到。
   ) //内部会第一次调用reducer函数，得到初始state为0
 export default store
