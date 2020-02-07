b59084d14ee75f00e4c8958a644c922cb3ce7ebc
HelenTim <3427439707@qq.com>
2020-02-07 10:09:58 +0800

    Update app.jsx

--- react-and-redux/src-redux-thunk/containers/app.jsx
+++ react-and-redux/src-redux-thunk/containers/app.jsx
@@ -1,4 +1,4 @@
-
+// 自注：react-redux的作用：主要是把数据和方法传递给需要的组件， 以便组件使用。
 import {connect} from 'react-redux' //连接redux和react组件
 import {increment,decrement,incrementAsync}  from '../redux/actions'
 import Counter from '../components/counter'
