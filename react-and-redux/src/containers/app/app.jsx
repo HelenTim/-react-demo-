9a939aaa17f0e48a5b42536290783e46858cf858
HelenTim <3427439707@qq.com>
2020-05-18 23:39:15 +0800

    对react-reudx的connect方法的理解：注入 数据/方法

--- react-and-redux/src/containers/app/app.jsx
+++ react-and-redux/src/containers/app/app.jsx
@@ -40,7 +40,7 @@ class App extends Component {
 
 export default connect( // 我们在使用<App/>组件的时候，发现并没有给组件传递addComments,deleteComments,getComments方法，coments数据也没有传进来，
                         // 但是发现却可以在组件里用props获取他们。原因就在这里，我们使用connect方法让store里面的comments数据和方法都传进来了。
-                        // 说白了：connect就是让数据和方法注入进最后一行声明的组件里。
+                        // 说白了：connect就是让数据和方法注入进最后一行声明的组件里。解耦作用：让公用的数据和方法不再和组件耦合（redux耦合性太强不好），而是直接注入。
   state => ({comments:state.comments}), //state包含comments数组。//尽管state是一个对象 代表redux定义了多个公用数据，但是我们整个应用只用到了comments这个数据 所以我们就只把这个数据绑定到这个应用里去。    
   {addComments,deleteComments,getComments}
 )(App);
